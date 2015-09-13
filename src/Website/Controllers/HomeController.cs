c76a7c7138fda1c7810df5f9e8424ae10fb51357
Matt Cosand <matt@cosand.net>
2015-09-12 17:56:20 -0700

    page for heartbeats

--- src/Website/Controllers/HomeController.cs
+++ src/Website/Controllers/HomeController.cs
@@ -4,20 +4,30 @@
 namespace Kcesar.MissionLine.Website.Controllers
 {
   using System.Web.Mvc;
+  using log4net;
 
+  [RequireHttps]
   public class HomeController : Controller
   {
     private readonly IConfigSource config;
+    private readonly ILog log;
 
-    public HomeController(IConfigSource config)
+    public HomeController(IConfigSource config, ILog log)
     {
       this.config = config;
+      this.log = log;
     }
 
     public ActionResult Index()
     {
       ViewBag.LinkTemplate = this.config.GetConfig("memberLinkTemplate");
       return View();
+    }
+
+    public ActionResult Heartbeat()
+    {
+      log.Info("Heartbeat");
+      return Content("OK");
     }
   }
 }
