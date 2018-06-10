04abe1b519abe702016b34a6a849dfdee667ccb3
Andrew Bares <anbare@microsoft.com>
2018-06-10 15:27:48 -0700

    Carpooling view and service initial code

---
+++ src/Website/Scripts/site/services/carpooling-service.js
@@ -0,0 +1,72 @@
+﻿angular.module('missionlineApp').service('carpoolingService', ['$sce', '$http', '$q', '$rootScope', '$timeout', 'pushService',
+  function ($sce, $http, $q, $rootScope, $timeout, pushService) {
+    var self = this;
+    $.extend(this, {
+      getCarpoolers: function (eventId) {
+        var deferred = $q.defer();
+        $http({
+          method: 'GET',
+          url: window.appRoot + 'api/events/' + eventId + '/carpoolers',
+        }).success(function (data) {
+          var carpoolers = [];
+          $.each(data, function (idx, carpooler) {
+            carpoolers.push(new CarpoolerModel(carpooler));
+          })
+          deferred.resolve(carpoolers);
+        })
+        .error(function (response) { deferred.reject(response); });
+        return deferred.promise;
+      },
+      updateCarpoolerInfo: function (eventId, currentMemberId, carpoolerInfo) {
+        var deferred = $q.defer();
+        $http({
+          method: 'POST',
+          url: window.appRoot + 'api/events/' + eventId + '/carpoolers/' + currentMemberId,
+          data: carpoolerInfo,
+          headers: { 'Content-Type': 'application/json' }
+        }).success(function (data) {
+          deferred.resolve(data);
+        }).error(function (response) {
+          deferred.reject(response);
+        });
+        return deferred.promise;
+      },
+      getUpdateInfo: function (eventId, memberId) {
+        var deferred = $q.defer();
+        $http({
+          method: 'GET',
+          url: window.appRoot + 'api/events/' + eventId + '/updateinfo/' + memberId,
+        }).success(function (data) {
+          deferred.resolve(data);
+        }).error(function (response) {
+          deferred.reject(response);
+        });
+        return deferred.promise;
+      },
+      removeCarpooler: function (eventId, memberId) {
+        var deferred = $q.defer();
+        $http({
+          method: 'DELETE',
+          url: window.appRoot + 'api/events/' + eventId + '/carpoolers/' + memberId,
+          headers: { 'Content-Type': 'application/json' }
+        }).success(function (data) {
+          deferred.resolve(data);
+        }).error(function (response) {
+          deferred.reject(response);
+        });
+        return deferred.promise;
+      },
+      getCarpooler: function (eventId, memberId) {
+        var deferred = $q.defer();
+        $http({
+          method: 'GET',
+          url: window.appRoot + 'api/events/' + eventId + '/carpoolers/' + memberId,
+        }).success(function (data) {
+          deferred.resolve(data);
+        }).error(function (response) {
+          deferred.reject(response);
+        });
+        return deferred.promise;
+      }
+    });
+  }]);
