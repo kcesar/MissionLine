0c8586e4b569ba2cfd17179d245c6fe32fd19831
Andrew Bares <anbare@microsoft.com>
2018-06-10 22:05:17 -0700

    Add some db models

---
+++ src/Website/Api/Model/CarpoolerEntry.cs
@@ -0,0 +1,42 @@
+﻿using Kcesar.MissionLine.Website.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kcesar.MissionLine.Website.Api.Model
+{
+  public class CarpoolerEntry
+  {
+    public CarpoolerEntry(Carpooler data)
+    {
+      CanBeDriver = data.CanBeDriver;
+      CanBePassenger = data.CanBePassenger;
+      LocationLatitude = data.LocationLatitude;
+      LocationLongitude = data.LocationLongitude;
+      VehicleDescription = data.VehicleDescription;
+      Message = data.Message;
+    }
+
+    public CarpoolerEntry() { }
+
+    public MemberLookupResult Member { get; set; }
+
+    public bool? CanBeDriver { get; set; }
+
+    public bool? CanBePassenger { get; set; }
+
+    public decimal? LocationLatitude { get; set; }
+
+    public decimal? LocationLongitude { get; set; }
+
+    public string VehicleDescription { get; set; }
+
+    public string Message { get; set; }
+
+    /// <summary>
+    /// TODO: Only initialized when viewing a specific contact? Maybe we could do this all via JS?
+    /// </summary>
+    public List<PersonContact> PersonContacts { get; set; }
+  }
+}
---
+++ src/Website/Data/Carpooler.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Spatial;
+using System.Linq;
+using System.Web;
+
+namespace Kcesar.MissionLine.Website.Data
+{
+  public class Carpooler
+  {
+    [Key, Column(Order = 0)]
+    public int EventId { get; set; }
+
+    [Key, Column(Order = 1)]
+    public string MemberId { get; set; }
+
+    /// <summary>
+    /// A member can choose to be a driver, or driver and passenger
+    /// </summary>
+    public bool CanBeDriver { get; set; }
+
+    /// <summary>
+    /// A member can choose to be a passenger, or passenger and driver
+    /// </summary>
+    public bool CanBePassenger { get; set; }
+
+    // Would use DbGeography spacial type, but it failed because Microsoft.SqlServer.Types version 10 or higher could not be found, and installing the NuGet package didn't work
+    public decimal LocationLatitude { get; set; }
+
+    public decimal LocationLongitude { get; set; }
+
+    /// <summary>
+    /// Their vehicle description, like "White 2000 Toyota 4Runner"
+    /// </summary>
+    public string VehicleDescription { get; set; } = "";
+
+    /// <summary>
+    /// A custom message they'd like displayed, in plain text.
+    /// </summary>
+    public string Message { get; set; } = "";
+  }
+}
